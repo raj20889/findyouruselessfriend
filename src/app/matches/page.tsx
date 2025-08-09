89824fd4d2fed81a5f4e53a4cbffa1f0560aeb11
Mohit Raj <rajm20889@gmail.com>
2025-08-09 09:54:19 +0000

    instead of percentage show the numbers

--- src/app/matches/page.tsx
+++ src/app/matches/page.tsx
@@ -15,14 +15,14 @@ const data = [
 ];
 
 const RADIAN = Math.PI / 180;
-const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }: any) => {
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, value }: any) => {
   const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
   const x = cx + radius * Math.cos(-midAngle * RADIAN);
   const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
   return (
     <text x={x} y={y} fill="white" textAnchor="middle" dominantBaseline="central" className="text-sm font-bold">
-      {`${(percent * 100).toFixed(0)}%`}
+      {value}
     </text>
   );
 };
